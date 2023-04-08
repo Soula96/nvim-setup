342b842330810247643da5f362d8e0e6a8297c4f
Soula96 <max.prause.96@gmail.com>
2023-04-09 00:58:20 +0200

    Added keymap integration for Neotree <leader> to access git

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,6 +1,5 @@
 
 vim.cmd("Neotree")
--- Another Test
---
-vim.cmd("Neotree")
 
+vim.cmd([[nnoremap <leader>b :Neotree toggle show buffers right<cr> ]])
+vim.cmd([[nnoremap <leader>g :Neotree float git_status<cr> ]] )
